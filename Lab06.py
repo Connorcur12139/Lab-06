a7289fa1c2ac048ee3cbd5dfc98c249e5426fad2
ConnorCurcio <curcioc@ufl.edu>
2022-10-31 13:22:00 -0400

    first commit

---
+++ Lab06.py
@@ -0,0 +1,15 @@
+##Program to add two numbers
+
+#Menu
+
+first = int(input('Please enter first number: '))
+second = int(input('Please enter second number: '))
+
+print('1. Addition')
+choice = int(input())
+
+
+if choice == 1:
+    print(first + second)
+
+
