f9888da5becf5c6fbcbfa4b0d230d5b2cea4c7a8
ConnorCurcio <curcioc@ufl.edu>
2022-10-31 13:29:55 -0400

    second commit

--- Lab06.py
+++ Lab06.py
@@ -6,10 +6,12 @@ first = int(input('Please enter first number: '))
 second = int(input('Please enter second number: '))
 
 print('1. Addition')
+print('2. Subtraction')
 choice = int(input())
 
 
 if choice == 1:
     print(first + second)
-
+if choice == 2:
+    print(first - second)
 
